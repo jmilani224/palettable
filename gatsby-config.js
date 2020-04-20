4dc05c64d0c9286cf0d752cb8384749a64689f6a
jmilani224 <jmilani224@gmail.com>
2020-04-20 02:14:34 -0400

    first netlify deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
       },
     },
   ],
