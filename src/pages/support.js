51897ab2d2182331239267de0a26c2b091178142
jmilani224 <jmilani224@gmail.com>
2020-04-24 13:48:43 -0400

    text edit

--- src/pages/support.js
+++ src/pages/support.js
@@ -8,7 +8,7 @@ const SupportPage = () => (
   <Layout>
     <Content>
       <h2>Support This Site</h2>
-      <p>Every developer has a monster bookmarks folder - endless lists of resources, articles, tools, and inspiration. This site is an attempt to collect and organize some of those bookmarks in a way that's easy to browse, contribute to, and share. If you have a site you'd like to add to the list, check out the <Link to="/contribute">Contribute</Link> section. And if you've found the site useful, you can always send a coffee my way.</p>
+      <p>This site is an attempt to collect and organize an unweildy bookmarks folder in a way that's easy to browse, contribute to, and share. If you have a site you'd like to add to the list, check out the <Link to="/contribute">Contribute</Link> section. And if you've found the site useful, you can always send a coffee my way.</p>
       <p>Thanks for visiting!</p>
       <CoffeeBtn />
     </Content>
